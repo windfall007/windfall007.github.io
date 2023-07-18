6fc89eed94eedc7440254240483c8126f97da687
jq <you@example.com>
2023-07-18 11:20:00 +0800

    Site updated: 2023-07-18 11:19:59

--- js/universe.js
+++ js/universe.js
@@ -1,11 +1,11 @@
-// 创建Universe类
-function Universe(el='universe'){
-
   // 获取requestAnimationFrame方法
   window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-  
+
+// 创建Universe类
+function Universe(el = 'universe') {
+
   // 星星密度
   var starDensity = .216;
 
@@ -13,7 +13,7 @@ function Universe(el='universe'){
   var speedCoeff = .05;
 
   // 宽度
-  var width;  
+  var width;
 
   // 高度
   var height;
@@ -54,7 +54,7 @@ function Universe(el='universe'){
 
   // 绑定窗口改变事件
   window.addEventListener('resize', windowResizeHandler, false);
-  
+
 
   // 创建星空
   createUniverse();
@@ -77,7 +77,7 @@ function Universe(el='universe'){
 
   // 每帧绘制
   function draw() {
-    
+
     // 清除画布
     universe.clearRect(0, 0, width, height);
 
@@ -111,8 +111,8 @@ function Universe(el='universe'){
   function Star() {
 
     // 重置星星状态
-    this.reset = function() {
-      
+    this.reset = function () {
+
       // 是否为巨星
       this.giant = getProbability(3);
 
@@ -153,7 +153,7 @@ function Universe(el='universe'){
     };
 
     // 渐现
-    this.fadeIn = function() {
+    this.fadeIn = function () {
       if (this.fadingIn) {
         this.fadingIn = this.opacity > this.opacityTresh ? false : true;
         this.opacity += this.do;
@@ -161,7 +161,7 @@ function Universe(el='universe'){
     };
 
     // 渐隐
-    this.fadeOut = function() {
+    this.fadeOut = function () {
       if (this.fadingOut) {
         this.fadingOut = this.opacity < 0 ? false : true;
         this.opacity -= this.do / 2;
@@ -175,7 +175,7 @@ function Universe(el='universe'){
     };
 
     // 绘制
-    this.draw = function() {
+    this.draw = function () {
       universe.beginPath();
 
       if (this.giant) {
@@ -201,7 +201,7 @@ function Universe(el='universe'){
     };
 
     // 移动
-    this.move = function() {
+    this.move = function () {
       this.x += this.dx;
       this.y += this.dy;
       if (this.fadingOut === false) {
@@ -214,8 +214,8 @@ function Universe(el='universe'){
     };
 
     // 首次运行后设置first为false
-    (function() {
-      setTimeout(function() {
+    (function () {
+      setTimeout(function () {
         first = false;
       }, 50)
     })()
@@ -246,4 +246,115 @@ function Universe(el='universe'){
     canva.setAttribute('height', height);
   }
 
+}
+
+/**
+ * 旋转的星空
+ * @param {*} el 元素
+ * @param {*} width 画布宽
+ * @param {*} height 画布高
+ */
+function Rotating_stars(el='rotating_stars',width=window.innerWidth,height=window.innerHeight) {
+  //宇宙特效
+  var canvas = document.getElementById(el),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width = width,
+    h = canvas.height = height,
+
+    hue = 217,
+    stars = [],
+    count = 0,
+    maxStars = 1300;//星星数量
+
+  var canvas2 = document.createElement('canvas'),
+      ctx2 = canvas2.getContext('2d');
+    canvas2.width = 100;
+    canvas2.height = 100;
+  var half = canvas2.width / 2,
+    gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+    gradient2.addColorStop(0.025, '#CCC');
+    gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+    gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+    gradient2.addColorStop(1, 'transparent');
+
+    ctx2.fillStyle = gradient2;
+    ctx2.beginPath();
+    ctx2.arc(half, half, half, 0, Math.PI * 2);
+    ctx2.fill();
+
+  // End cache
+
+  function random(min, max) {
+    if (arguments.length < 2) {
+      max = min;
+      min = 0;
+    }
+
+    if (min > max) {
+      var hold = max;
+      max = min;
+      min = hold;
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function maxOrbit(x, y) {
+    var max = Math.max(x, y),
+      diameter = Math.round(Math.sqrt(max * max + max * max));
+    return diameter / 2;
+    //星星移动范围，值越大范围越小，
+  }
+
+  var Star = function () {
+
+    this.orbitRadius = random(maxOrbit(w, h));
+    this.radius = random(60, this.orbitRadius) / 8;
+    //星星大小
+    this.orbitX = w / 2;
+    this.orbitY = h / 2;
+    this.timePassed = random(0, maxStars);
+    this.speed = random(this.orbitRadius) / 50000;
+    //星星移动速度
+    this.alpha = random(2, 10) / 10;
+
+    count++;
+    stars[count] = this;
+  }
+
+  Star.prototype.draw = function () {
+    var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+      y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+      twinkle = random(10);
+
+    if (twinkle === 1 && this.alpha > 0) {
+      this.alpha -= 0.05;
+    } else if (twinkle === 2 && this.alpha < 1) {
+      this.alpha += 0.05;
+    }
+
+    ctx.globalAlpha = this.alpha;
+    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+    this.timePassed += this.speed;
+  }
+
+  for (var i = 0; i < maxStars; i++) {
+    new Star();
+  }
+
+  function animation() {
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.globalAlpha = 0.5; //尾巴
+    ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)';
+    ctx.fillRect(0, 0, w, h)
+
+    ctx.globalCompositeOperation = 'lighter';
+    for (var i = 1, l = stars.length; i < l; i++) {
+      stars[i].draw();
+    };
+
+    window.requestAnimationFrame(animation);
+  }
+
+  animation();
 }
